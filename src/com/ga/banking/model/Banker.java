80314e91203922f8cde62ecf036cedce607afedf
Mohamed Husain <mo.abdulla3000@gmail.com>
2025-12-02 07:51:11 -0800

    Create base Customer and Banker classes

---
+++ src/com/ga/banking/model/Banker.java
@@ -0,0 +1,15 @@
+package com.ga.banking.model;
+
+public class Banker extends Person {
+
+    public Banker(String username, String password) {
+        super(username,password);
+
+    }
+
+    public void addCustomer(Customer customer){
+
+    }
+
+
+}
---
+++ src/com/ga/banking/model/Customer.java
@@ -0,0 +1,9 @@
+package com.ga.banking.model;
+
+public class Customer extends Person{
+
+    public Customer(String username, String password){
+        super(username,password);
+    }
+
+}
